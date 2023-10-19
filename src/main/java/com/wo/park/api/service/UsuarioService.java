25bf406a4edc72f0efb472ce03940859c4394ac8
wanderson648 <testepedroca@gmail.com>
2023-10-19 17:43:46 -0300

    implementa metodos para controller

--- src/main/java/com/wo/park/api/service/UsuarioService.java
+++ src/main/java/com/wo/park/api/service/UsuarioService.java
@@ -1,8 +1,11 @@
 package com.wo.park.api.service;
 
+import com.wo.park.api.entity.Usuario;
 import com.wo.park.api.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -10,4 +13,23 @@ public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
 
+
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public Usuario buscarPorId(Long id) {
+        return usuarioRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuario não encontrado"));
+    }
+
+    public Usuario editarSenha(Long id, String password) {
+        Usuario user = buscarPorId(id);
+        user.setPassword(password);
+        return usuarioRepository.save(user);
+    }
+
+    public List<Usuario> buscarTodos() {
+        return usuarioRepository.findAll();
+    }
 }
--- src/main/java/com/wo/park/api/web/controller/UsuarioController.java
+++ src/main/java/com/wo/park/api/web/controller/UsuarioController.java
@@ -1,9 +1,14 @@
 package com.wo.park.api.web.controller;
 
+import com.wo.park.api.entity.Usuario;
 import com.wo.park.api.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.catalina.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,4 +16,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(usuarioService.salvar(usuario));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getUserById(@PathVariable Long id) {
+        Usuario user = usuarioService.buscarPorId(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Usuario> updatePassword(@PathVariable Long id, @RequestBody Usuario usuario) {
+        Usuario user = usuarioService.editarSenha(id, usuario.getPassword());
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> getAll() {
+        List<Usuario> usuarios = usuarioService.buscarTodos();
+        return ResponseEntity.ok(usuarios);
+    }
 }
