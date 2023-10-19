5b15abf396fef5df1ee9cc319f9f858bf91ee0f3
wanderson648 <testepedroca@gmail.com>
2023-10-19 15:30:55 -0300

    configura banco e timezone do projeto

---
+++ src/test/java/com/wo/park/api/DemoParkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wo.park.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoParkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
