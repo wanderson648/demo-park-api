4e81046e5dc89050304c72ef39803e5cca077383
wanderson648 <testepedroca@gmail.com>
2023-10-24 12:53:25 -0300

    implementa exceptions handler

--- src/main/java/com/wo/park/api/service/UsuarioService.java
+++ src/main/java/com/wo/park/api/service/UsuarioService.java
@@ -1,8 +1,11 @@
 package com.wo.park.api.service;
 
 import com.wo.park.api.entity.Usuario;
+import com.wo.park.api.exception.EntityNotFoundException;
+import com.wo.park.api.exception.UsernameUniqueViolationException;
 import com.wo.park.api.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,21 +18,26 @@ public class UsuarioService {
 
 
     public Usuario salvar(Usuario usuario) {
-        return usuarioRepository.save(usuario);
+        try {
+            return usuarioRepository.save(usuario);
+        } catch (DataIntegrityViolationException ex) {
+            throw new UsernameUniqueViolationException(
+                    String.format("Username {%s} já cadastrado", usuario.getUsername()));
+        }
     }
 
     public Usuario buscarPorId(Long id) {
         return usuarioRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Usuario não encontrado"));
+                .orElseThrow(() -> new EntityNotFoundException(String.format("Usuário id={%s} não encontrado", id)));
     }
 
     public Usuario editarSenha(Long id, String senhaAtual, String novaSenha, String confirmaSenha) {
-        if(!novaSenha.equals(confirmaSenha)) {
+        if (!novaSenha.equals(confirmaSenha)) {
             throw new RuntimeException("Nova senha não confere com confirmação de senha");
         }
 
         Usuario user = buscarPorId(id);
-        if(!user.getPassword().equals(senhaAtual)) {
+        if (!user.getPassword().equals(senhaAtual)) {
             throw new RuntimeException("Sua senha não confere");
         }
 
